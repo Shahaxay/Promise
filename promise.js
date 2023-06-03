62d1fc1bdace112ad8553030718ec114d449ee56
Akshayshah47 <removeit61@gmail.com>
2023-06-03 21:51:24 +0530

    setting timeout in updateLastUserActivityTime and add delete function

--- promise.js
+++ promise.js
@@ -1,13 +1,15 @@
 var posts=[];
 var user={
     name:"user name",
-    lastActivity:new Date()
+    lastActivity:new Date(2023,3,20)
 };
 console.log(`Before creating Post${posts.length+1}, user last activity time=`,user.lastActivity);
 function updateLastUserActivityTime(){
     return new Promise((resolve,reject)=>{
-        user.lastActivity=new Date();
-        resolve();
+        setTimeout(()=>{
+            user.lastActivity=new Date();
+            resolve();
+        },1000);
     })
 }
 
@@ -15,18 +17,39 @@ function createPost(post){
     return new Promise((resolve,reject)=>{
         posts.push(post);
         setTimeout(()=>{
-            updateLastUserActivityTime().then(resolve);
+            resolve();
         },1000);
     })
 }
 
-var promise1=createPost({title:"post1"});
-var promise2=createPost({title:"post1"});
+function deletePost(){
+    return new Promise((resolve,reject)=>{
+        const deletedItme=posts.pop();
+        resolve(deletedItme);
+    })
+}
 
-Promise.all([promise1,promise2]).then(()=>{
+function displayPost(){
     console.log(`After creating post${posts.length}>>>`);
     console.log(posts);
     console.log(`user last activity time ${user.lastActivity}`);
+}
+
+var promise1=createPost({title:"post1"});
+var promise2=updateLastUserActivityTime();
+
+Promise.all([promise1,promise2]).then(()=>{
+    displayPost();
+    var promise3=createPost({title:"post2"});
+    var promise4=updateLastUserActivityTime();
+    Promise.all([promise3,promise4]).then(()=>{
+        displayPost();
+        deletePost().then((post)=>{
+            console.log(`After deleting last post:${post.title}`);
+            console.log(posts);
+        });
+    })
 })
+
 
 
